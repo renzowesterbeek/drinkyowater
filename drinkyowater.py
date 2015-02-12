185290610ebd90d6a4347fa60fd9150f9679c4cb
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-02-12 18:18:46 +0100

    Move main loop to function

--- drinkyowater.py
+++ drinkyowater.py
@@ -13,16 +13,20 @@ def yo_all():
 def currentTime():
 	return time.strftime("%H:%M")
 
-notificationTimes = ["7:20", "7:30" "11:10", "13:10", "15:12", "16:12", "17:00", "19:00",  "20:00",  "21:00",  "21:30"]
-lastCheck = ""
-
 # Main program loop
-while True:
-	print currentTime()
+def main():
+	notificationTimes = ["7:20", "7:30" "11:10", "13:10", "15:12", "16:12", "17:00", "19:00",  "20:00",  "21:00",  "21:30"]
+	lastCheck = ""
 	
-	if currentTime() != lastCheck:	
-		if currentTime() in notificationTimes:
-			print yo_all()
-			lastCheck = currentTime()
-	
-	time.sleep(20)
+	while True:
+		print currentTime()
+		
+		if currentTime() != lastCheck:	
+			if currentTime() in notificationTimes:
+				# print yo_all()
+				print "yo_all()"
+				lastCheck = currentTime()
+		
+		time.sleep(20)
+		
+main()
