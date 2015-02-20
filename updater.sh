0f25fa897a1e700c37f16b4547c716aead596337
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-02-20 19:15:22 +0100

    Remove directory echoing and revert back to remoteSHA

--- updater.sh
+++ updater.sh
@@ -3,8 +3,8 @@ function checkForUpdate {
 	localSHA=$(git rev-parse HEAD)
 	remoteSHA=$(git ls-remote git://github.com/renzowesterbeek/drinkyowater.git HEAD)
 	
-	# remoteSHA10=${remoteSHA:0:10}
-	remoteSHA10=$(cat sha.txt)
+	# remoteSHA10=$(cat sha.txt)
+	remoteSHA10=${remoteSHA:0:10}
 	localSHA10=${localSHA:0:10}
 	
 	echo $localSHA10
@@ -17,9 +17,7 @@ function checkForUpdate {
 		cd ..
 		rm -rf drinkyowater/
 		git clone https://github.com/renzowesterbeek/drinkyowater.git
-		ls
 		cd drinkyowater/
-		ls
 		echo "Done!"
 	fi
 }
