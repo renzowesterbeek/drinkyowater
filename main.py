ab2cfb7f8a7d1254192226e38630f6b92393029d
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-02-21 09:10:49 +0100

    Fix weekday bug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def yo_all():
 # Returns list with times based on day
 def getListOfTheDay():
 	w = time.strftime("%w")
-	if w == 0 or w == 6: # Weekend
+	if w == "0" or w == "6": # Weekend
 		return ["10:30", "12:30", "13:30", "14:30", "16:30", "18:30", "20:30", "21:30"]
 	else: # Weekday
 		return ["07:20", "07:30", "11:10", "13:10", "15:12", "16:12", "17:00", "19:00",  "20:00",  "21:00",  "21:30"]
